c40a8b8f049eb1d1453c36bd3aa2ae1ce38fe585
truefusion <truefusion@gmail.com>
2024-06-02 15:45:41 -0400

    Documentation.

---
+++ src/stories/components/Divider.stories.js
@@ -0,0 +1,10 @@
+import divider_horizontal from './html/divider/index.html?raw';
+import divider_vertical from './html/divider/vertical.html?raw';
+
+export default {
+	title: 'Components/Divider',
+}
+
+export const Divider = () => divider_horizontal;
+
+export const DividerVertical = () => divider_vertical;
